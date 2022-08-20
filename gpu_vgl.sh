65e73bade138af8e819595c971925ed2d89ce42a
bkmz12332 <45734789+bkmz12332@users.noreply.github.com>
2022-08-20 10:00:02 +0300

    Create gpu_vgl.sh

---
+++ gpu_vgl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat <<EOF > /etc/profile.d/gpu_vgl.sh
+#!/bin/bash
+gpu(){
+
+declare -a MEMORY_LIST=( $(nvidia-smi -q -d MEMORY  | grep -C 3 FB | grep Used | awk '{print $3}') )
+max=${MEMORY_LIST[0]}
+min=${MEMORY_LIST[0]}
+
+for i in ${MEMORY_LIST[*]}; do
+  ((x=x+1))
+  let q=$x-1
+  (( i >= max )) && max=$i
+  (( i <= min )) && min=$i && j=$q
+done
+
+export VGL_DISPLAY=":0.$j" ; vglrun $@
+
+#    declare -p GPU_LIST
+    MEMORY_LIST=()
+unset q
+unset g
+unset x
+unset i
+unset j
+}
+EOF
